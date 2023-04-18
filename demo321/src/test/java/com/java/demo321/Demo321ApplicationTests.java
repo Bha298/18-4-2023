1d7bc45ffd37915021de1c21954577de9ce55f47
Bha298 <79588388+Bha298@users.noreply.github.com>
2023-04-18 12:14:57 +0530

    Add files via upload

---
+++ demo321/src/test/java/com/java/demo321/Demo321ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.demo321;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo321ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
